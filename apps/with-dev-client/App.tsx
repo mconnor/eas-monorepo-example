fa2dc4e79d845ce86819b9a352301a11c0c6263f
Cedric van Putten <me@bycedric.com>
2021-10-25 14:52:29 +0200

    chore: fix all eslint issues with --fix

--- apps/with-dev-client/App.tsx
+++ apps/with-dev-client/App.tsx
@@ -1,15 +1,15 @@
-import { Paragraph, Strong } from "@acme/ui";
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { Button, StyleSheet, TextInput, View } from "react-native";
-import { MMKV, useMMKVString } from "react-native-mmkv";
+import { Paragraph, Strong } from '@acme/ui';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { MMKV, useMMKVString } from 'react-native-mmkv';
 
 const storage = new MMKV();
 
 export default function App() {
-  const [usernameInput, setUsernameInput] = useState("");
+  const [usernameInput, setUsernameInput] = useState('');
 
-  const [username, setUsername] = useMMKVString("user.name", storage);
+  const [username, setUsername] = useMMKVString('user.name', storage);
 
   return (
     <View style={styles.container}>
@@ -17,7 +17,7 @@ export default function App() {
         Hello from an <Strong>EAS</Strong> monorepo (with-dev-client)
       </Paragraph>
       <Paragraph>
-        Your MMKV username is <Strong>{username ?? "unknown"}</Strong>
+        Your MMKV username is <Strong>{username ?? 'unknown'}</Strong>
       </Paragraph>
       <TextInput
         placeholder="Type your username here"
@@ -25,10 +25,7 @@ export default function App() {
         onChangeText={(u) => setUsernameInput(u)}
         style={styles.input}
       />
-      <Button
-        title="Save"
-        onPress={() => usernameInput.length && setUsername(usernameInput)}
-      />
+      <Button title="Save" onPress={() => usernameInput.length && setUsername(usernameInput)} />
       <StatusBar style="auto" />
     </View>
   );
@@ -37,9 +34,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   input: {
     fontSize: 20,
