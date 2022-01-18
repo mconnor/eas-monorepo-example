575bb9aeff57e3ce8de1afe754d952f0138f5359
mike connor <mike@mikeconnor.tech>
2022-01-18 16:07:35 -0600

    formatting

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,38 +1,38 @@
-import { ReactNode } from "react";
-import { StyleSheet, Text, TextProps } from "react-native";
+import { ReactNode } from 'react'
+import { StyleSheet, Text, TextProps } from 'react-native'
 
 export interface ParagraphProps extends TextProps {
-  children?: ReactNode;
+    children?: ReactNode
 }
 
 export function Paragraph({ children, style, ...props }: ParagraphProps) {
-  return (
-    <Text {...props} style={[styles.paragraph, style]}>
-      {children}
-    </Text>
-  );
+    return (
+        <Text {...props} style={[styles.paragraph, style]}>
+            {children}
+        </Text>
+    )
 }
 
 export interface StrongProps extends TextProps {
-  children?: ReactNode;
+    children?: ReactNode
 }
 
 export const Strong = ({ children, style, ...props }: StrongProps) => (
-  <Text {...props} style={[styles.strong, style]}>
-    {children}
-  </Text>
-);
+    <Text {...props} style={[styles.strong, style]}>
+        {children}
+    </Text>
+)
 
 const styles = StyleSheet.create({
-  paragraph: {
-    color: "rgba(0, 0, 0, 0.87)",
-    fontSize: 14,
-    letterSpacing: 0.25,
-    lineHeight: 20,
-    marginVertical: 2,
-  },
-  strong: {
-    fontWeight: "bold",
-    color: "rgb(0, 0, 238)",
-  },
-});
+    paragraph: {
+        color: 'rgba(0, 0, 0, 0.87)',
+        fontSize: 14,
+        letterSpacing: 0.25,
+        lineHeight: 20,
+        marginVertical: 2
+    },
+    strong: {
+        fontWeight: 'bold',
+        color: 'rgb(0, 0, 238)'
+    }
+})
