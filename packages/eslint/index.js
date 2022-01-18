659f6723dee6b727715c66bbb15c26da28ce034a
mike connor <mike@mikeconnor.tech>
2022-01-18 11:12:32 -0600

    eslint 8

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,7 +1,8 @@
 module.exports = {
-  extends: 'eslint-config-universe',
-  // do some additional things with it
-  rules: {
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-  },
-};
+    extends: ['universe/native', 'prettier'],
+
+    // do some additional things with it
+    rules: {
+        'prettier/prettier': ['error', { endOfLine: 'auto' }]
+    }
+}
