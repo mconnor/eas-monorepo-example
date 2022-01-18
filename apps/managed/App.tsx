9f84e768adb91c19edfd3778aca72efa6201a81a
mike connor <mike@mikeconnor.tech>
2022-01-18 16:06:13 -0600

    prettierrc at top

--- apps/managed/App.tsx
+++ apps/managed/App.tsx
@@ -1,23 +1,23 @@
-import { Paragraph, Strong } from "@acme/ui";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { Paragraph, Strong } from '@acme/ui'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Paragraph>
-        Hello from an <Strong>EAS</Strong> monorepo (managed)
-      </Paragraph>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Paragraph>
+                Hello from an <Strong>EAS</Strong> monorepo (managed)
+            </Paragraph>
+            <StatusBar style="auto" />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
